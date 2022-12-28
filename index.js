b8f23616b4580ed8d59bf78a90df18cbe49d12d5
Shirin A S <83756732+shirinas2002@users.noreply.github.com>
2022-12-28 06:36:28 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const express = require("express");
-const app= express();
-const mongoose = require('mongoose')
-const UserModel = require('./models/Users')
-mongoose.connect("mongodb://localhost:27017/merndb");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app.get("/getusers", (req,res) => {
-    UserModel.find({} , (err, result,) =>{
-        console.log(result,err)
-        if (err) {
-            res.json(err);
-        }else {
-            res.json(result);
-
-        }
-    })
-})
-
-
-app.listen(3001,() => {
-    console.log("Hello")
-});
